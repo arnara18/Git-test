2559b40702ecf5fb2d7df837164176e432f1114e
arnara18 <arnara18@ru.is>
2018-09-04 10:31:57 +0000

    this is the second test for max_int code!

--- max_int.py
+++ max_int.py
@@ -1,17 +1,12 @@
-# take input from user: num_int = int(input("Input a number: "))   
+# take input from user: 
+num_int = int(input("Input a number: "))   
+max_int = 0
 
 # check if integer is positive 
-
-#if positive the input is the max
-
-#take another input
-
-#check if positive 
-
-#if positive, check if input is > max_int
-
-#if input > max_int, input is new max_int
-
-#if number negitive -> print max_int
- 
-#print("The maximum is", max_int)    # Do not change this line
+while num_int > 0: #if positive, the first input is the max
+    if max_int < num_int:
+        max_int = num_int
+    num_int = int(input("Input a number: "))
+            
+#if number negitive then print max_int
+print("The maximum is", max_int)    # Do not change this line
